dbf608154cc9ccab2ed4401c7a0fd3aa27b50ded
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2024-06-07 02:19:11 +0800

    [upload] 10382 Greedy Algorithm (Interval Cover) Implement(AC) change vector type
    Signed-off-by: bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>

--- alg10/10382_greedy.cpp
+++ alg10/10382_greedy.cpp
@@ -7,30 +7,38 @@ using namespace std;
 //#define in cin
 //#define out cout
 
+void print_vector(vector<tuple<double, double, double>> &v, ofstream &out) {
+    for (auto &i: v) {
+        out << get<0>(i) << " " << get<1>(i) << " " << get<2>(i) << endl;
+    }
+}
+
 int main() {
     ifstream in("./alg10/10382.in");
     ofstream out("./alg10/10382.out");
     int n;
     double l, w;
     while (in >> n >> l >> w) {
-        vector<pair<double, double>> v;
+        vector<tuple<double, double, double>> v;
         for (int i = 0; i < n; i++) {
             int x, r;
             in >> x >> r;
             if (r * 2 >= w) {
                 double d = sqrt(r * r - w * w / 4);
-                v.push_back({x - d, x + d});
+                v.push_back({x - d, x + d, x});
             }
         }
         sort(v.begin(), v.end());
+//        print_vector(v, out);
         int ans = 0;
         int i = 0;
         int j = 0;
         double cur = 0;
         while (i < v.size() && cur < l) {
             double r = cur;
-            while (j < v.size() && v[j].first <= cur) {
-                r = max(r, v[j].second);
+            while (j < v.size() && get<0>(v[j]) <= cur) {
+                r = max(r, get<1>(v[j]));
+//                out<<r<<endl;
                 j++;
             }
             if (r == cur) {
